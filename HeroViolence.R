fd5dfb12a49997615ca7f14ee2789b92c42c3b8f
Joe Hilgard <jhilgard@gmail.com>
2015-01-11 09:06:31 -0600

    Added effect size code and contingencyTableBF.

--- HeroViolence.R
+++ HeroViolence.R
@@ -18,6 +18,10 @@ dat$DV = dat$Calls; dat$DV[dat$DV >= 1] = 1
 chisq.test(dat$DV, y=interaction(dat$Game, dat$Request))
 model = glm(DV ~ Game*Request, family="binomial", data=dat)
 summary(model)
+ORs = data.frame("OR"=exp(summary(model)$coefficients[,1]),
+                 "LL"=exp(summary(model)$coefficients[,1]-1.98*summary(model)$coefficients[,2]),
+                 "UL"=exp(summary(model)$coefficients[,1]+1.98*summary(model)$coefficients[,2])
+)
 # Doesn't look like it. Let's have the means.
 means = tapply(dat$DV, INDEX=list(dat$Game, dat$Request), FUN=mean, na.rm=T)
 barplot(means, beside=T, legend.text=c("Control", "Antisocial", "Hero")
@@ -41,3 +45,16 @@ barplot(meansTrim, beside=T)
 # Just how much usable data is there?
 sum(complete.cases(data.frame(dat$DV, dat$Game, dat$Request)))
 sum(complete.cases(data.frame(trim$DV, trim$Game, trim$Request)))
+
+# Bayes?
+require(BayesFactor)
+tab = table(dat$DV, dat$Game, dat$Request)
+bf = contingencyTableBF(tab, sampleType="indepMulti", fixedMargin = "cols")
+bf
+tab2 = table(dat$DV, dat$Game)
+bf2 = contingencyTableBF(tab2, sampleType="indepMulti", fixedMargin="cols")
+bf2
+# Future directions:
+# Convert OR to r for Bayesian model comparison with Dienes calculator
+# http://www.meta-analysis.com/downloads/Meta-analysis%20Converting%20among%20effect%20sizes.pdf
+# http://www.campbellcollaboration.org/escalc/html/EffectSizeCalculator-R6.php
