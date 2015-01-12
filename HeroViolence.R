10c4ab1bbe172e0524eb3b2e3a3e40044862bb7e
Joe Hilgard <jhilgard@gmail.com>
2015-01-11 20:22:40 -0600

    minor updates

--- HeroViolence.R
+++ HeroViolence.R
@@ -22,6 +22,7 @@ ORs = data.frame("OR"=exp(summary(model)$coefficients[,1]),
                  "LL"=exp(summary(model)$coefficients[,1]-1.98*summary(model)$coefficients[,2]),
                  "UL"=exp(summary(model)$coefficients[,1]+1.98*summary(model)$coefficients[,2])
 )
+d = OR.to.d(b=summary(model)$coefficients[,1])
 # Doesn't look like it. Let's have the means.
 means = tapply(dat$DV, INDEX=list(dat$Game, dat$Request), FUN=mean, na.rm=T)
 barplot(means, beside=T, legend.text=c("Control", "Antisocial", "Hero")
@@ -38,6 +39,14 @@ summary(modelCalls) #again nothing
 trim = dat[as.numeric(row.names(dat)) < 187,]
 modelTrim = glm(DV ~ Game*Request, family="binomial", data=trim)
 summary(modelTrim)
+ORsTrim = data.frame("OR"=exp(summary(modelTrim)$coefficients[,1]),
+                 "LL"=exp(summary(modelTrim)$coefficients[,1]-1.98*summary(modelTrim)$coefficients[,2]),
+                 "UL"=exp(summary(modelTrim)$coefficients[,1]+1.98*summary(modelTrim)$coefficients[,2])
+)
+dTrim = OR.to.d(b=summary(modelTrim)$coefficients[,1])
+# how much does removing that data change d
+dTrim - d
+
 # Doesn't look like it. Let's have the means.
 meansTrim = tapply(trim$DV, INDEX=list(trim$Game, trim$Request), FUN=mean, na.rm=T)
 barplot(meansTrim, beside=T)
@@ -54,7 +63,19 @@ bf
 tab2 = table(dat$DV, dat$Game)
 bf2 = contingencyTableBF(tab2, sampleType="indepMulti", fixedMargin="cols")
 bf2
+
+# Drop the interactions
+model2 = glm(DV ~ Game + Request, data=dat, family="binomial")
+summary(model2)
+d2 = OR.to.d(b=summary(model2)$coefficients[,1])
+
+model2Trim = glm(DV ~ Game + Request, data=trim, family="binomial")
+summary(model2Trim)
+d2Trim = OR.to.d(b=summary(model2Trim)$coefficients[,1])
+
+d2Trim - d2
 # Future directions:
 # Convert OR to r for Bayesian model comparison with Dienes calculator
 # http://www.meta-analysis.com/downloads/Meta-analysis%20Converting%20among%20effect%20sizes.pdf
 # http://www.campbellcollaboration.org/escalc/html/EffectSizeCalculator-R6.php
+
